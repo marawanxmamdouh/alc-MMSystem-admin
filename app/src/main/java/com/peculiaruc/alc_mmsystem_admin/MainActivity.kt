a2e48a79c0edbc263714ecdc792ed29a0223ba7f
Peculiaruc <peculiarumeh02@gmail.com>
2022-11-04 10:06:59 +0100

    First commit

---
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.peculiaruc.alc_mmsystem_admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
