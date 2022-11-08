f3e74eeb37b57875ebe19e3535b4c9a7bee6f9dd
Nada Feteiha <nada.feteiha@gmail.com>
2022-11-08 07:26:25 -0800

    implement bottom navigation design

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/main/MainActivity.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/main/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.peculiaruc.alc_mmsystem_admin.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.peculiaruc.alc_mmsystem_admin.R
+import com.peculiaruc.alc_mmsystem_admin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val appBarConfiguration = AppBarConfiguration(
+        setOf(
+            R.id.homeFragment,
+            R.id.searchFragment,
+            R.id.programsFragment,
+            R.id.messagesFragment,
+            R.id.adminProfileFragment
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
     }
 }
