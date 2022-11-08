5df7271975538f3cabf0521e70fcaea22a0ea34c
Nada Feteiha <nada.feteiha@gmail.com>
2022-11-08 00:16:59 -0800

    implement admin profile design

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/adminProfile/AdminProfileFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/adminProfile/AdminProfileFragment.kt
@@ -1,22 +1,26 @@
 package com.peculiaruc.alc_mmsystem_admin.ui.adminProfile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.peculiaruc.alc_mmsystem_admin.R
+import com.peculiaruc.alc_mmsystem_admin.databinding.FragmentAdminProfileBinding
 
 
 class AdminProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentAdminProfileBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_profile, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_admin_profile, container, false)
+        return binding.root
     }
-
 }
