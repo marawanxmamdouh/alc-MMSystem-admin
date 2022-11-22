c84fd0d2162286c14fe1ba08463362f3b82f64de
Nada Feteiha <nada.feteiha@gmail.com>
2022-11-22 10:59:50 -0800

    navigation to notification for home

--- app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_admin/ui/home/HomeViewModel.kt
@@ -1,6 +1,16 @@
 package com.peculiaruc.alc_mmsystem_admin.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.peculiaruc.alc_mmsystem_admin.utilities.event.Event
 
 class HomeViewModel : ViewModel() {
+
+    private val _notificationEvent = MutableLiveData<Event<Boolean>>()
+    val notificationEvent: LiveData<Event<Boolean>> = _notificationEvent
+
+    fun onNotificationClick() {
+        _notificationEvent.postValue(Event(true))
+    }
 }
